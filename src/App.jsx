bfe7665955e1c69e576a719acd817f910f4964b7
askhat <0x.ultrasound@gmail.com>
2023-05-03 20:24:00 +0600

    --01
    update package-lock.json: add "modern-normalize": "^1.1.0", "styled-components": "^5.3.10";
    delete index.css

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { GlobalStyle } from 'components/GlobalStyle';
+
+export const App = () => {
+  return (
+    <div>
+      <GlobalStyle />
+      React homework template ttt
+    </div>
+  );
+};
